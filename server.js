2cb596b02ba433419ed3220ac49b9e928ced9cdb
Steven Sammon <stevenesammon@hotmail.com>
2018-01-13 17:43:12 -0500

    app listening on PORT 8000

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8000;
+
+
+
+
+
+app.listen(PORT, function(){
+
+    console.log("Listening on PORT" + PORT);
+
+});
