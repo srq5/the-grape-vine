fc5396215f47f554453c6aa5bff24738de7e3218
Steven Sammon <stevenesammon@hotmail.com>
2018-01-15 15:22:04 -0500

    adds basic api routes

---
+++ controller/api-routes.js
@@ -0,0 +1,38 @@
+var db = require("../models");
+
+app.get("/api/wines", function(req, res){
+
+
+});
+
+app.post("api/wines", function(req, res){
+
+});
+
+
+app.put("api/wines", function (req, res){
+
+    db.Wine.findAll().then(function(results){
+        res.json(results);
+    })
+
+});
+
+
+
+app.get("/api/users", function (req, res){
+
+});
+
+app.post("api/users", function (req, res){
+
+});
+
+app.put("/api/users", function(req, res){
+
+})
+
+app.delete("/api/users", function (req, res){
+
+});
+
