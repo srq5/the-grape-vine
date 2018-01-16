445a18705bd207942fac75a4de77837d76e81c09
Steven Sammon <stevenesammon@hotmail.com>
2018-01-15 20:00:42 -0500

    creates MVP html routes with placeholder path names

---
+++ controller/html-routes.js
@@ -0,0 +1,17 @@
+var path = require("path");
+
+module.exports = function(app){
+
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "#"));
+    });
+
+    app.get("/profile", function(req, res){
+        res.sendFile(path.join(__dirname, "#"));
+    });
+
+    app.get("/wines", function(req, res){
+        res.sendFile(path.join(__dirname, "#"));
+    });
+
+}
