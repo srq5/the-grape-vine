00751228e1937ab8596446f69c07dd1bd16b5cc7
Steven Sammon <stevenesammon@hotmail.com>
2018-01-16 11:22:37 -0500

    replaces placeholder with real html path

--- controller/html-routes.js
+++ controller/html-routes.js
@@ -3,15 +3,15 @@ var path = require("path");
 module.exports = function(app){
 
     app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "#"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/profile", function(req, res){
-        res.sendFile(path.join(__dirname, "#"));
+        res.sendFile(path.join(__dirname, "../public/profiles.html"));
     });
 
-    app.get("/wines", function(req, res){
-        res.sendFile(path.join(__dirname, "#"));
+    app.get("/reviews", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/reviews.html"));
     });
 
 }
