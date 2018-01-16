bd487d7519b31b03730774b99c0d60670d96c586
Steven Sammon <stevenesammon@hotmail.com>
2018-01-15 19:37:46 -0500

    adds basic Wine model

--- models/wine.js
+++ models/wine.js
@@ -1,12 +1,76 @@
-module.exports = function(sequelize, Datatypes) {
+module.exports = function(sequelize, DataTypes) {
 
     var Wine = sequelize.define("Wine", {
+
+        country: {
+            
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+        description: {
+
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+
+        designation: {
+
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+
+        points: {
+
+        },
+        price: {
+
+        },
+        province: {
+
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+        region_1: {
+
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+
+        region_2: {
+
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+
+        variety: {
+
+            type: DataTypes.STRING,
+            allowNull: false
+
+        },
+
+        winery: {
+
+            type: DataTypes.STRING,
+            allowNull: false
+
+        }
+
+
 
 
 
 
 
     });
+
+    return Wine;
 
 
 
