ecd056c6107f23ee1bcce7376f6c0c1f05ae291c
Steven Sammon <stevenesammon@hotmail.com>
2018-01-17 14:44:33 -0500

    creates one to many association from User to Wine model

--- models/user.js
+++ models/user.js
@@ -44,6 +44,15 @@ module.exports = function(sequelize, DataTypes){
             allowNull: false
         }
 
-    })
+    });
+
+
+    User.associate = function(models){
+        User.hasMany(models.Wine, {
+          onDelete: "CASCADE"
+        });
+      }
+
+
 return User;
 }
