968953576bf28e2fdbecaa62a350d453770e6601
Ovie <you@example.com>
2018-01-17 16:17:36 -0500

    updates new review and update review api routes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,31 @@
+//new wine constructor
+function NewWine(title, country,description,price,points,variety,winery){
+	this.title= title,
+	this.country = country,
+	this.description=description,
+	this.price=price,
+	this.points=points,
+	this.variety=variety,
+	this.winery=winery
+}
+
 //on clicking add wine button
 $("#addWine").on("click", function(e){
 	e.preventdefault();
 	//open modal with add form
+	var wineTitle = $("#addFormTitle").val().trim();
+	var wineCountry = $("#addFormCountry").val().trim();
+	var wineDesc = $("#addFormDesc").val().trim();
+	var winePrice = $("#addFormPrice").val().trim();
+	var winePts = $("#addFormPts").val().trim();
+	var wineVariety = $("#addFormVariety").val().trim();
+	var wineWinery = $("#adFormWinery").val().trim();
+	//create newWine object
+	var newWine = NewWine(wineTitle,wineCountry,wineDesc,winePrice,winePts,wineVariety,wineWinery);
+	//post newWine obj
+	$.post("/api/wines", newWine, function(data){
+		//display new wine
+	})
 });
 
 //on clicking remove wine button
@@ -14,3 +38,10 @@ $("#removeWine").on("click", function(e){
 		console.log("Wine deleted!");
 	})
 });
+
+//on clicking an update review button
+
+$("#updateReview").on("click", function(e){
+	e.preventdefault();
+
+})
