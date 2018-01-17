54f2ce65e622cdd2a59071e414f7c0d560ff8258
Steven Sammon <stevenesammon@hotmail.com>
2018-01-17 14:34:25 -0500

    creates User API CRUD routes

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -9,27 +9,7 @@ app.get("/api/wines", function(req, res){
 
 });
 
-app.get("/api/variety/:variety", function(req, res){
-    db.Wine.findAll({
-        where:{
-            variety: req.params.variety
-        }
-    }).then(function(results){
-        res.json(results);
-    });
-});
-
-app.get("/api/country/:country", function(req, res){
-    db.Wine.findAll({
-        where: {
-            country: req.params.country
-        }
-    }).then(function(results){
-        res.json(results);
-    });
-});
-
-app.post("/api/wines", function(req, res){
+app.post("api/wines", function(req, res){
 
     db.Wine.create({
 
@@ -39,7 +19,7 @@ app.post("/api/wines", function(req, res){
         price: req.body.price,
         points: req.body.points,
         variety: req.body.variety,
-        winery: req.body.winery
+        winery: req.body.winery,
         
 
     }).then(function(results){
@@ -49,58 +29,66 @@ app.post("/api/wines", function(req, res){
 });
 
 
-app.put("/api/wines", function (req, res){
-    db.Wine.update({
-        title: req.body.title,
-        country: req.body.country,
-        description: req.body.description,
-        price: req.body.price,
-        points: req.body.points,
-        variety: req.body.variety,
-        winery: req.body.winery
-    }, {
-        where: {
-            id: req.body.id
-        }
-    }).then(function(results){
-        res.json(results);
-    });
+app.put("api/wines", function (req, res){
+
     
 
 });
 
-app.delete("/api/wines/:id", function(req, res){
-    db.Wine.destroy({
+app.delete("api/wines", )
+
+
+
+
+
+
+
+
+
+
+app.get("/api/user/:username", function (req, res){
+
+    db.User.findOne({
         where: {
-            id: req.params.id
+            username: req.params.username
         }
     }).then(function(results){
-        response.json(results);
+        res.json(results);
     });
-});
-
-
-
-
-
-
-
-
-
-
-app.get("/api/users/:user", function (req, res){
 
 });
 
-app.post("api/users", function (req, res){
+app.post("api/user", function (req, res){
+
+    db.User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        username: req.body.username,
+        favorite_wines: req.body.favorite_wines
+    }).then(function(results){
+        res.json(results);
+    });
 
 });
 
-app.put("/api/users", function(req, res){
+app.put("/api/user", function(req, res){
+
+    db.User.update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        username: req.body.username,
+        favorite_wines: req.body.favorite_wines
+    }).then(function(results){
+        res.json(results);
+    });
 
 })
 
-app.delete("/api/users", function (req, res){
+app.delete("/api/user", function (req, res){
 
 });
 
