2149a14620358c2f49ff74a6dc513d83c17e4ca0
Steven <stevenesammon@hotmail.com>
2018-01-19 14:55:41 -0500

    comments out all api routes except findAll wines

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -3,10 +3,16 @@ var db = require("../models");
 
 module.exports =function(app){
     
-app.get("/api/wines", function(req, res){
+app.get("/reviews", function(req, res){
 
-    db.Wine.findAll().then(function(results){
-        res.json(results);
+    db.Wine.findAll({
+
+        where: {
+            points: gt(99)
+        }
+    })
+    
+    .then(function(results){
         //render in reviews page
         res.render("reviews", {wines:results});
     });
@@ -14,92 +20,93 @@ app.get("/api/wines", function(req, res){
 
 });
 
+// app.get("/api/variety/:variety", function(req, res){
+//     db.Wine.findAll({
+//         where:{
+//             variety: req.params.variety
+//         }
+//     }).then(function(results){
+//         res.json(results);
+//         //render in reviews page
+//     });
+// });
 
-app.get("/api/variety/:variety", function(req, res){
-    db.Wine.findAll({
-        where:{
-            variety: req.params.variety
-        }
-    }).then(function(results){
-        res.json(results);
-        //render in reviews page
-    });
-});
+// app.get("/api/country/:country", function(req, res){
+//     db.Wine.findAll({
+//         where: {
+//             country: req.params.country
+//         }
+//     }).then(function(results){
+//         res.json(results);
+//         //render in reviews page
+//     });
+// });
 
-app.get("/api/country/:country", function(req, res){
-    db.Wine.findAll({
-        where: {
-            country: req.params.country
-        }
-    }).then(function(results){
-        res.json(results);
-        //render in reviews page
-    });
-});
+// app.post("api/wines", function(req, res){
 
-app.post("api/wines", function(req, res){
+//     db.Wine.create({
 
-    db.Wine.create({
-
-        title: req.body.title,
-        country: req.body.country,
-        description: req.body.description,
-        price: req.body.price,
-        points: req.body.points,
-        variety: req.body.variety,
-        winery: req.body.winery,
+//         title: req.body.title,
+//         country: req.body.country,
+//         description: req.body.description,
+//         price: req.body.price,
+//         points: req.body.points,
+//         variety: req.body.variety,
+//         winery: req.body.winery,
         
 
-    }).then(function(results){
-        res.json(results);
-        //render in reviews page
-    });
+//     }).then(function(results){
+//         res.json(results);
+//         //render in reviews page
+//     });
 
-});
+// });
 
 
-app.put("/api/wines", function (req, res){
-    db.Wine.update({
-        title: req.body.title,
-        country: req.body.country,
-        description: req.body.description,
-        price: req.body.price,
-        points: req.body.points,
-        variety: req.body.variety,
-        winery: req.body.winery
-    }, {
-        where: {
-            id: req.body.id
-        }
-    }).then(function(results){
-        res.json(results);
-        //render in reviews page
+// app.put("/api/wines", function (req, res){
+//     db.Wine.update({
+//         title: req.body.title,
+//         country: req.body.country,
+//         description: req.body.description,
+//         price: req.body.price,
+//         points: req.body.points,
+//         variety: req.body.variety,
+//         winery: req.body.winery
+//     }, {
+//         where: {
+//             id: req.body.id
+//         }
+//     }).then(function(results){
+//         res.json(results);
+//         //render in reviews page
 
-    });
+//     });
     
 
-});
+// });
 
-app.get("api/wines/:id", function(req, res){
-    db.Wine.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then(function(results){
-    res.json(results);
-    //render in reviews page
-    })
-})
+// app.get("api/wines/:id", function(req, res){
+//     db.Wine.findOne({
+//         where: {
+//             id: req.params.id
+//         },
 
-app.delete("/api/wines/:id", function(req, res){
-    db.Wine.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(function(results){
-        res.json(results);
-    });
-});
+//         include: [db.Review]
+//     }).then(function(results){
+//     res.json(results);
+//     //render in reviews page
+//     })
+// })
+
+// app.delete("/api/wines/:id", function(req, res){
+//     db.Wine.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(function(results){
+//         res.json(results);
+//     });
+// });
 
 // app.get("/api/user/:id", function(req, res){
 
@@ -110,61 +117,64 @@ app.delete("/api/wines/:id", function(req, res){
 
 // })
 
-app.get("/api/user/:username", function (req, res){
+// app.get("/api/user/:username", function (req, res){
 
-    db.User.findOne({
-        where: {
-            username: req.params.username
-        }
-    }).then(function(results){
-        res.json(results);
-        //render in profile page
-    });
+//     db.User.findOne({
+//         where: {
+//             username: req.params.username
+//         },
+//         include: [db.Wine],
+//         include: [db.Review]
 
-});
+//     }).then(function(results){
+//         res.json(results);
+//         //render in profile page
+//     });
 
-app.post("api/user", function (req, res){
+// });
 
-    db.User.create({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: req.body.password,
-        username: req.body.username,
+// app.post("api/user", function (req, res){
+
+//     db.User.create({
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         email: req.body.email,
+//         password: req.body.password,
+//         username: req.body.username,
        
-    }).then(function(results){
-        res.json(results);
-    });
+//     }).then(function(results){
+//         res.json(results);
+//     });
 
-});
+// });
 
-app.put("/api/user", function(req, res){
+// app.put("/api/user", function(req, res){
 
-    db.User.update({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: req.body.password,
-        username: req.body.username,
-    }).then(function(results){
-        res.json(results);
-    });
+//     db.User.update({
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         email: req.body.email,
+//         password: req.body.password,
+//         username: req.body.username,
+//     }).then(function(results){
+//         res.json(results);
+//     });
 
-})
+// })
 
-//User can only delete from their own profile.
+// //User can only delete from their own profile.
 
-app.delete("/api/user/:username", function (req, res){
+// app.delete("/api/user/:username", function (req, res){
 
-    db.User.destroy({
+//     db.User.destroy({
 
-        where: {
-            username: req.params.username
-        }
+//         where: {
+//             username: req.params.username
+//         }
         
-    }).then(function(results){
-        res.json(results);
-    });
+//     }).then(function(results){
+//         res.json(results);
+//     });
 
-});
+// });
 }
